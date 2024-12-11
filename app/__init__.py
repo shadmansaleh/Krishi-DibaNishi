1e4fadd84dceaa18f44671aed4a4531f3c4b84c7
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2024-12-12 02:34:32 +0600

    add protected route+404 page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.routes.views import pages, register_pages
@@ -16,11 +16,17 @@ def create_app():
 
     # Register the Blueprint
     app.register_blueprint(pages)
-    
+
     from .routes.auth import auth
     app.register_blueprint(auth)
     app.register_blueprint(api)
 
     register_pages(app)
+
+    # Catch-all route for 404 errors
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template("errors/404.html"), 404
+
     return app
 

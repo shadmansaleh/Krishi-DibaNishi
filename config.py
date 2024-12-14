09dcd5f9495eac40a463bb5cfacebfc268fd9652
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2024-12-14 17:30:35 +0600

    make more deployment friendly

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    env = os.getenv("FLASK_ENV", "development")
+    if env == "production":
+        port = int(os.getenv("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
+    else:
+        app.run(debug=True)

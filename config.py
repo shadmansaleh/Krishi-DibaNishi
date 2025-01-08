2d6d968c359dd7db1cb74acbd5c9a0f18aed79fd
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-01-09 00:14:36 +0600

    add proper auth for db

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_db_uri():
+    db_uri = os.getenv('DATABASE_URI') or ""
+    if "{DB_AUTH_TOKEN}" in db_uri and os.getenv('DB_AUTH_TOKEN'):
+       return db_uri.replace('{DB_AUTH_TOKEN}', os.getenv('DB_AUTH_TOKEN') or "")
+    return db_uri
+
 
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = get_db_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv('FLASK_ENV') != 'production'

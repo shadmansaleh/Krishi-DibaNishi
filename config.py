12841e68e9817fe852ef2c7a792727cbc0c098bc
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-01-08 13:24:07 +0600

    turn off debug mode in prod

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    DEBUG = os.getenv('FLASK_ENV') != 'production'
